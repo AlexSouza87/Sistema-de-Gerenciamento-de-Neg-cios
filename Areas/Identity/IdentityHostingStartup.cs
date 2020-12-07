5b756379dfb7eb4aff9545d8a9d4d7f358f29e10
AlexSouza87 <61298270+AlexSouza87@users.noreply.github.com>
2020-12-06 22:40:58 -0300

    Tela de login e cadastro de usuarios feita

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SistemaGerenciadorDeNegocios.Areas.Identity.Data;
+using SistemaGerenciadorDeNegocios.Data;
+
+[assembly: HostingStartup(typeof(SistemaGerenciadorDeNegocios.Areas.Identity.IdentityHostingStartup))]
+namespace SistemaGerenciadorDeNegocios.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<SistemaGerenciadorDeNegociosContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("SistemaGerenciadorDeNegociosContextConnection")));
+
+                services.AddDefaultIdentity<SistemaGerenciadorDeNegociosUser>(options => options.SignIn.RequireConfirmedAccount = false)
+                    .AddEntityFrameworkStores<SistemaGerenciadorDeNegociosContext>();
+            });
+        }
+    }
+}
